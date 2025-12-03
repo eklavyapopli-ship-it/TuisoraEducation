308964aeefba3851e53d9bff45e1f2ead01ec387
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-03 23:00:18 +0530

    UI changed

--- app/(home)/hero.tsx
+++ app/(home)/hero.tsx
@@ -1,13 +1,15 @@
 "use client"
 import { motion } from 'framer-motion'
+import { TypeAnimation } from "react-type-animation";
 const Hero = () => {
   return (
-    <div className='flex flex-col lg:flex-row place-content-center place-items-center  md:justify-center lg:space-x-30 space-y-10 lg:mt-20 mt-10   '>
+    <section className='w-full flex justify-center'>
+    <div className='flex flex-col lg:flex-row place-content-center place-items-center  md:justify-center lg:space-x-30 space-y-10 lg:mt-20 mt-10  max-w-6xl'>
         <motion.div
          style={{overflow:"hidden"}}
         initial={{x:-180,opacity:0}}
         animate={{x:0,opacity:1}}
-        transition={{duration:1,ease:"easeIn"}}>
+        transition={{duration:0.5,ease:"easeIn"}}>
             <img src="/logo2.png" className='lg:h-80 lg:w-100 h-50 w-60' alt="" />
         </motion.div>
         <motion.div
@@ -15,20 +17,20 @@ const Hero = () => {
         style={{overflow:"hidden"}}
         initial={{x:180,opacity:0}}
         animate={{x:0,opacity:1}}
-        transition={{duration:1,ease:"easeIn"}}>
-        <motion.div 
-        style={{overflow:"hidden", whiteSpace:"nowrap"}} 
-        initial={{width:0}}
-        animate={{ width:"100%"}}
-        transition={{duration:1,ease:"easeIn"}}
-
-        className='text-black font-bold lg:text-5xl text-2xl h-15 place-content-center text-center'>
-            Next-Gen Learning Starts Here
-        </motion.div>
+        transition={{duration:0.5,ease:"easeIn"}}>
+        <div className='text-xl md:text-2xl font-bold text-gray-900 mb-6'>
+       <TypeAnimation
+      sequence={["Next-Gen Learning Starts Here",1000]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    /></div>
         <motion.div
-        className='text-black text-sm md:text-xl text-center'>Interactive lessons, mock tests, and personalized guidance for every student.</motion.div>
+        className='text-black text-sm md:text-xl md:text-justify text-center '>Interactive lessons, mock tests, and personalized guidance for every student.</motion.div>
         </motion.div>
     </div>
+    </section>
   )
 }
 
