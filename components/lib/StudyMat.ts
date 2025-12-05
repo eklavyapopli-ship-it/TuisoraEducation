59a3ee514f3c8abac87720246c82e4887f45fa85
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-05 11:23:11 +0530

    study material objects added

---
+++ components/lib/StudyMat.ts
@@ -0,0 +1,44 @@
+interface MaterialCards{
+    id: number;
+    heading:string;
+    subHead:string;
+    color:string;
+}
+export const MaterialCards = [
+    {
+        id:1,
+        heading: "PREVIOUS YEAR",
+        subHead:"Question Papers",
+        color:"",
+    },
+    {
+        id:2,
+        heading:"SAMPLE",
+        subHead: "Question Papers",
+        color:"",
+    },
+    {
+        id:3,
+        heading:"IMPORTANT",
+        subHead: "Questions",
+        color:"",
+    },
+    {
+        id:4,
+        heading:"REVISION",
+        subHead: "Notes",
+        color:"",
+    },
+    {
+        id:5,
+        heading:"GENERAL KNOWLEDGE",
+        subHead: "",
+        color:"",
+    },
+    {
+        id:6,
+        heading:"TIPS & TRICKS",
+        subHead: "",
+        color:"",
+    },
+]
