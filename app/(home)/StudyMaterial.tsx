cfd67efcac988640d2e896e3623ab8ff88767619
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-05 23:23:47 +0530

    animations added

--- app/(home)/StudyMaterial.tsx
+++ app/(home)/StudyMaterial.tsx
@@ -1,19 +1,37 @@
 "use client"
 import { MaterialCards } from "@/components/lib/StudyMat"
 import { Josefin_Sans } from 'next/font/google'
+import { motion } from "framer-motion"
 const josefin = Josefin_Sans({
-    subsets: ['latin'],
-  })
+  subsets: ['latin'],
+})
 const StudyMaterial = () => {
-const card = MaterialCards;
+  const card = MaterialCards;
   return (
-    <section className={`${josefin.className} h-[80vh]`}>
-      <div className="text-center text-black text-4xl lg:text-6xl font-bold">STUDY <span className="text-blue-600">MATERIAL</span></div>
+    <section className={`${josefin.className} mt-20`}>
+      <motion.div
+      style={{overflow:"hidden"}}
+      initial={{y:-40,opacity:0}}
+      animate={{y:0}}
+      whileInView={{opacity:1}}
+      transition={{duration:0.5,ease:"easeInOut"}}
+      className="text-center text-black text-4xl lg:text-6xl font-bold">STUDY <span className="text-blue-600">MATERIAL</span></motion.div>
       <div className="w-full h-fit flex flex-col place-items-center">
         <div className="place-items-center md:grid md:grid-cols-4 flex flex-col gap-x-25 gap-y-20 m-20">
-        {card.map((item)=>(
-          <div key={item.id} style={{background:item.color}} className="h-80 w-60 relative shadow-sm rounded-2xl">{}</div>
-      ))}</div></div>
+          {card.map((item) => (
+            <motion.div
+            style={{overflow:"hidden",background:item.color}}
+      initial={{y:-40,opacity:0}}
+      animate={{y:0}}
+      whileInView={{opacity:1}}
+      whileHover={{scale:1.03}}
+      transition={{duration:0.2,ease:"easeInOut"}}
+            key={item.id} className="h-80 w-60 relative shadow-sm flex flex-col place-items-center text-black font-bold text-md pt-10 rounded-2xl">
+              <div>{item.heading}</div>
+              <div className="text-md font-medium text-gray-800">{item.subHead}</div>
+
+            </motion.div>
+          ))}</div></div>
     </section>
   )
 }
