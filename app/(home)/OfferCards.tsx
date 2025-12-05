8e732419909f48839aaa32e75c1e181bac1ddcc0
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-05 11:31:49 +0530

    animations added

--- app/(home)/OfferCards.tsx
+++ app/(home)/OfferCards.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+"use client"
 import { Josefin_Sans } from 'next/font/google'
 import { OfferHeads } from '@/components/lib/headings'
-import { div, h1 } from 'framer-motion/client'
+import { TypeAnimation } from 'react-type-animation'
+import { motion } from 'framer-motion'
 const josefin = Josefin_Sans({
   subsets: ['latin'],
 })
 const OfferCards = () => {
   return (
     <section className='w-screen h-[80vh]'>
-      <div className={`text-center lg:text-6xl text-4xl font-bold text-black ${josefin.className}`}>What We <span className='text-blue-600'>Offer</span></div>
+      <motion.div
+      style={{overflow:"hidden"}}
+      initial={{y:-40,opacity:0}}
+      animate={{y:0,opacity:1}}
+      transition={{duration:1,ease:"easeInOut"}}
+      className={`text-center lg:text-6xl text-4xl font-bold text-black ${josefin.className} `}>What We <span className='text-blue-600'>Offer</span></motion.div>
       <div className='w-full h-fit flex justify-center lg:space-y-0 space-y-5  mt-15 lg:space-x-8 lg:mt-20 lg:flex-row flex-col lg:place-content-center place-items-center'>
         {OfferHeads.map((item)=>(
           <div className='flex flex-col lg:h-120 h-fit rounded-2xl border-blue-400 place-items-center border py-8 lg:w-100 w-80  px-15 space-y-5' key={item.id}>
