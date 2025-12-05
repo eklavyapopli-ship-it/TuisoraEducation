65b81f24d389bcf5c17689ca8f791fb26b669bcf
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-05 23:30:11 +0530

    responsivenss increased

--- app/(home)/OfferCards.tsx
+++ app/(home)/OfferCards.tsx
@@ -8,7 +8,7 @@ const josefin = Josefin_Sans({
 })
 const OfferCards = () => {
   return (
-    <section className='w-screen h-[80vh]'>
+    <section className='w-screen h-fit'>
       <motion.div
       style={{overflow:"hidden"}}
       initial={{y:-40,opacity:0}}
@@ -16,7 +16,7 @@ const OfferCards = () => {
       transition={{duration:1,ease:"easeInOut"}}>
       <div
       className={`text-center lg:text-6xl text-4xl font-bold text-black ${josefin.className} `}>What We <span className='text-blue-600'>Offer</span></div>
-      <div className='w-full h-fit flex justify-center lg:space-y-0 space-y-5  mt-15 lg:space-x-8 lg:mt-20 lg:flex-row flex-col lg:place-content-center place-items-center'>
+      <div className='w-full h-fit grid justify-center lg:space-y-0 space-y-5  mt-15 lg:space-x-8  space-x-0 lg:mt-20 lg:grid-cols-3 grid-cols-1 lg:place-content-center place-items-center'>
         {OfferHeads.map((item)=>(
           <div className='flex flex-col lg:h-120 h-fit rounded-2xl border-blue-600 place-items-center border py-8 lg:w-100 w-80  px-15 space-y-5' key={item.id}>
             <span className='text-black font-bold'>{item.name}</span>
