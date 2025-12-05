bbdf4809e3751a57adc7a2f9be64e861f7723553
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-05 23:41:11 +0530

    link object added

--- app/(home)/StudyMaterial.tsx
+++ app/(home)/StudyMaterial.tsx
@@ -2,6 +2,7 @@
 import { MaterialCards } from "@/components/lib/StudyMat"
 import { Josefin_Sans } from 'next/font/google'
 import { motion } from "framer-motion"
+import Link from "next/link"
 const josefin = Josefin_Sans({
   subsets: ['latin'],
 })
@@ -19,6 +20,7 @@ const StudyMaterial = () => {
       <div className="w-full h-fit flex flex-col place-items-center mt-20 md:mt-0">
         <div className="place-items-center md:grid lg:grid-cols-4 md:grid-cols-3 flex flex-col gap-x-25 gap-y-20 md:m-20">
           {card.map((item) => (
+            <Link target="_blank" key={item.id} href={item.link}>
             <motion.div
             style={{overflow:"hidden",background:item.color}}
       initial={{y:-40,opacity:0}}
@@ -26,11 +28,11 @@ const StudyMaterial = () => {
       whileInView={{opacity:1}}
       whileHover={{scale:1.03}}
       transition={{duration:0.2,ease:"easeInOut"}}
-            key={item.id} className="h-80 w-60 relative shadow-sm flex flex-col place-items-center text-black font-bold text-md pt-10 rounded-2xl">
+             className="h-80 w-60 relative shadow-sm flex flex-col place-items-center text-black font-bold text-md pt-10 rounded-2xl">
               <div>{item.heading}</div>
               <div className="text-md font-medium text-gray-800">{item.subHead}</div>
 
-            </motion.div>
+            </motion.div></Link>
           ))}</div></div>
     </section>
   )
--- components/lib/StudyMat.ts
+++ components/lib/StudyMat.ts
@@ -3,40 +3,47 @@ interface MaterialCards{
     heading:string;
     subHead:string;
     color:string;
+    link:string;
 }
 export const MaterialCards = [
     {
         id:1,
+        link:"",
         heading: "PREVIOUS YEAR",
         subHead:"Question Papers",
         color:"rgb(248,216,247)",
     },
     {
         id:2,
+        link:"",
         heading:"SAMPLE",
         subHead: "Question Papers",
         color:"rgb(219,246,254)",
     },
     {
         id:3,
+        link:"",
         heading:"IMPORTANT",
         subHead: "Questions",
         color:"rgb(250,255,216)",
     },
     {
         id:4,
+        link:"",
         heading:"REVISION",
         subHead: "Notes",
         color:"rgb(248,214,213)",
     },
     {
         id:5,
+        link:"",
         heading:"GENERAL KNOWLEDGE",
         subHead: "",
         color:"rgb(221,254,232)",
     },
     {
         id:6,
+        link:"",
         heading:"TIPS & TRICKS",
         subHead: "",
         color:"rgb(221,254,221)",
