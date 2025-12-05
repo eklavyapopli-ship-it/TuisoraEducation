2a46a84de94a99cb59c48c6f374f01f7c429cac6
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-05 11:15:29 +0530

    colour changes

--- app/(home)/OfferCards.tsx
+++ app/(home)/OfferCards.tsx
@@ -8,10 +8,10 @@ const josefin = Josefin_Sans({
 const OfferCards = () => {
   return (
     <section className='w-screen h-[80vh]'>
-      <div className={`text-center lg:text-6xl text-4xl font-bold text-black ${josefin.className}`}>What We <span className='text-teal-400'>Offer</span></div>
+      <div className={`text-center lg:text-6xl text-4xl font-bold text-black ${josefin.className}`}>What We <span className='text-blue-600'>Offer</span></div>
       <div className='w-full h-fit flex justify-center lg:space-y-0 space-y-5  mt-15 lg:space-x-8 lg:mt-20 lg:flex-row flex-col lg:place-content-center place-items-center'>
         {OfferHeads.map((item)=>(
-          <div className='flex flex-col lg:h-120 h-fit rounded-2xl border-teal-400 place-items-center border py-8 lg:w-100 w-80  px-15 space-y-5' key={item.id}>
+          <div className='flex flex-col lg:h-120 h-fit rounded-2xl border-blue-400 place-items-center border py-8 lg:w-100 w-80  px-15 space-y-5' key={item.id}>
             <span className='text-black font-bold'>{item.name}</span>
             <h1 className='font-bold text-black text-2xl'>HOME TUITION</h1>
             <div className='lg:mt-5  space-y-3'>
