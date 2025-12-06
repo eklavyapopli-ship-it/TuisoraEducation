1f79f426d63cc42e689001da2f83fae527977237
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-06 11:57:34 +0530

    respnsiveness fixed

--- app/(home)/ReviewCards.tsx
+++ app/(home)/ReviewCards.tsx
@@ -29,7 +29,6 @@ const ReviewCards: React.FC<dataDisplay> = ({limit}) => {
 
 
     }, [])
-    const displayedData = data;
     
     const limitedData = showAll
     ? data.toReversed()
@@ -40,21 +39,21 @@ const ReviewCards: React.FC<dataDisplay> = ({limit}) => {
 
         <div className={`max-w-7xl mx-auto h-fit pb-20 mt-20 ${josefin.className} flex flex-col justify-center place-items-center`}>
             <h1 className={`md:text-5xl text-3xl text-center font-bold text-black ${josefin.className}`}>STUDENT <span className="text-blue-600">REVIEWS</span></h1>
-            <div className=" mt-20 lg:grid lg:grid-cols-2 gap-x-10 gap-y-10  place-items-center flex flex-col">
+            <div className=" mt-20 lg:grid lg:grid-cols-2 gap-x-10 gap-y-20  place-items-center flex flex-col">
                 {limitedData.map((item) => (
                     <motion.div
                     whileHover={{scale:1.03}}
-                     key={item._id} className={`h-60 p-10 md:w-150 w-fit border-black rounded-2xl hover:shadow-xl flex flex-col relative shadow-md text-black ${josefin.className}`}>
+                     key={item._id} className={`md:h-60 h-fit  p-10 md:w-150 gap-y-10 w-fit m-1 border-black rounded-2xl hover:shadow-xl flex flex-col relative shadow-md text-black ${josefin.className}`}>
                         <div className="flex place-items-end place-content-start flex-1 w-full gap-6"><img src="/userIcon.png" alt="" className="h-20 w-22" />
-                       <div className="mb-1"> <div className="text-xl">Name: {item.name}</div>
-                       <div className="text-md text-gray-700">Course: {item.course}</div>
+                       <div className="mb-1"> <div className="md:text-xl">Name: {item.name}</div>
+                       <div className="md:text-md text-sm text-gray-700">Course: {item.course}</div>
                        </div>
                         </div>
-                        <div className="text-center flex flex-col flex-1 place-content-center">"{item.review}"</div>
+                        <div className="text-center flex flex-col flex-1 place-content-center  text-sm">"{item.review}"</div>
                     </motion.div>
                 ))}
             </div>
-            <div className="m-20 text-center text-3xl text-blue-600 font-semibold cursor-pointer w-fit group relative"
+            <div className="m-20 text-center text-xl text-blue-600 font-semibold cursor-pointer w-fit group relative"
              onClick={()=>{
                 if(showAll==false)
                     {setShowAll(true)}
@@ -66,4 +65,4 @@ const ReviewCards: React.FC<dataDisplay> = ({limit}) => {
     )
 }
 
-export default ReviewCards
+export default ReviewCards;
